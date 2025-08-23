606ed431164fe338d5ebe5c4ea83da2367d4f496
HungYeager <123phuhung.dl@gmail.com>
2025-08-23 17:57:00 +0700

    Update route.ts

--- fe-arise/src/app/api/auth/[...nextauth]/route.ts
+++ fe-arise/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
 import { handlers } from "@/auth";
 
 export const { GET, POST } = handlers;
+
+// Thêm dòng này để buộc Vercel sử dụng môi trường Node.js cho route này
+export const runtime = "nodejs"
